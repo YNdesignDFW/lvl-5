f2290f3a622aaae89a05bac0457df09add4d4a22
bekindtomonkeys <jared.caraway@gmail.com>
2012-08-18 00:33:17 -0500

    Got pickle/PrettyPrint working.
    Unpickled the .p file and displayed the resulting data structure in
    readable form with PrettyPrint. Now trying to figure out how to proceed.

--- lvl-5.py
+++ lvl-5.py
@@ -1,2 +1,7 @@
-f = open('peakhell.b')
-print f
+import pickle, pprint
+from cStringIO import StringIO
+
+datPickle = pickle.load(open('C:/python27/pychallenge/lvl-5/banner.p','rb'))
+pp = pprint.PrettyPrinter()
+pp.pprint(datPickle)
+print datPickle
